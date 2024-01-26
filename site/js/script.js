20d850e871b093537f342a990ed265a141644c70
Arthur-Prates <arthurprateslima@gmail.com>
2024-01-26 14:40:22 -0300

    Alteração do layout do curriculo e animação do footer

---
+++ site/js/script.js
@@ -0,0 +1,76 @@
+
+t = document.getElementById('t')
+gith = document.getElementById('githubFooter')
+gitTextInfoContato = document.getElementById('gitTextInfoContato')
+gitIconInfoContato = document.getElementById('gitIconInfoContato')
+instaTextInfoContato = document.getElementById('instaTextInfoContato')
+instaIconInfoContato = document.getElementById('instaIconInfoContato')
+LinkedInIconInfoContato = document.getElementById('LinkedInIconInfoContato')
+LinkedInTextInfoContato = document.getElementById('LinkedInTextInfoContato')
+gith.addEventListener('mouseover', function () {
+    t.style.display = "flex";
+    t.style.transition = "all 0.1s ease-in-out";
+})
+gith.addEventListener('mouseout', function () {
+    t.style.display = "none";
+    t.style.transition = "all 0.1s ease-in-out";
+})
+t.addEventListener('mouseover', function () {
+    t.style.display = "flex";
+    t.style.transition = "all 0.1s ease-in-out";
+})
+t.addEventListener('mouseout', function () {
+    t.style.display = "none"; 
+    t.style.transition = "all 0.1s ease-in-out";
+})
+gitIconInfoContato.addEventListener('mouseover', function () {
+    gitTextInfoContato.style.display = "flex";
+    gitTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+gitIconInfoContato.addEventListener('mouseout', function () {
+    gitTextInfoContato.style.display = "none";
+    gitTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+gitTextInfoContato.addEventListener('mouseover', function () {
+    gitTextInfoContato.style.display = "flex";
+    gitTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+gitTextInfoContato.addEventListener('mouseout', function () {
+    gitTextInfoContato.style.display = "none";
+    gitTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+
+instaIconInfoContato.addEventListener('mouseover', function () {
+    instaTextInfoContato.style.display = "flex";
+    instaTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+instaIconInfoContato.addEventListener('mouseout', function () {
+    instaTextInfoContato.style.display = "none";
+    instaTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+instaTextInfoContato.addEventListener('mouseover', function () {
+    instaTextInfoContato.style.display = "flex";
+    instaTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+instaTextInfoContato.addEventListener('mouseout', function () {
+    instaTextInfoContato.style.display = "none";
+    instaTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+
+LinkedInIconInfoContato.addEventListener('mouseover', function () {
+    LinkedInTextInfoContato.style.display = "flex";
+    LinkedInTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+LinkedInIconInfoContato.addEventListener('mouseout', function () {
+    LinkedInTextInfoContato.style.display = "none";
+    LinkedInTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+LinkedInTextInfoContato.addEventListener('mouseover', function () {
+    LinkedInTextInfoContato.style.display = "flex";
+    LinkedInTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+LinkedInTextInfoContato.addEventListener('mouseout', function () {
+    LinkedInTextInfoContato.style.display = "none";
+    LinkedInTextInfoContato.style.transition = "all 0.1s ease-in-out";
+})
+
